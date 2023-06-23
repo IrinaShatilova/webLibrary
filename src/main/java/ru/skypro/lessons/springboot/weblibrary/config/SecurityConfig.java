0f01406c859197677da65e7b269c54404e34127e
shati <shatilova_ira@mail.ru>
2023-06-24 00:33:32 +0600

    Spring Security начало

---
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package ru.skypro.lessons.springboot.weblibrary.config;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static ru.skypro.lessons.springboot.weblibrary.model.Role.ADMIN;
+import static ru.skypro.lessons.springboot.weblibrary.model.Role.USER;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return http.csrf(AbstractHttpConfigurer::disable)
+                        .formLogin(Customizer.withDefaults())
+                        .logout(Customizer.withDefaults())
+                        .sessionManagement(Customizer.withDefaults())
+                        .httpBasic(Customizer.withDefaults())
+                        .authorizeHttpRequests(this::customiseRequest)
+                        .build();
+        }
+
+        @SneakyThrows
+        public void customiseRequest(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+                registry.requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole(ADMIN.name())
+                        .requestMatchers(new AntPathRequestMatcher("/**")).hasAnyRole(USER.name(), ADMIN.name());
+        }
+
+        @Bean
+        @SneakyThrows
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+                return configuration.getAuthenticationManager();
+
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+}
+
